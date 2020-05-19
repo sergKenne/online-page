0f8c96cec8207444f7b96ffc47451b6a4cc8796f
Kenne Serge <kenne_serge@mail.ru>
2020-05-19 16:01:45 +0300

    last commit

--- portfolio/CRUDLOCALVUEMATERIALIZE/main.js
+++ portfolio/CRUDLOCALVUEMATERIALIZE/main.js
@@ -34,18 +34,24 @@ new Vue({
 		},
 		updateTask: function(e) {
 			e.preventDefault();
-			this.edit = !this.edit;
-			let taskdb = {
-				task: this.task,
-				description: this.description
-			};
 
-			this.tasks[this.ind] = taskdb;
-			localStorage.setItem('tasks', JSON.stringify(this.tasks));
-			let taskDB = JSON.parse(localStorage.getItem('tasks'));
-			this.tasks = taskDB;
-			this.task="";
-			this.description="";
+			if(this.task === "" || this.description ==="") {
+				alert("Please fill all fields");
+			} else {
+
+				this.edit = !this.edit;
+				let taskdb = {
+					task: this.task,
+					description: this.description
+				};
+
+				this.tasks[this.ind] = taskdb;
+				localStorage.setItem('tasks', JSON.stringify(this.tasks));
+				let taskDB = JSON.parse(localStorage.getItem('tasks'));
+				this.tasks = taskDB;
+				this.task="";
+				this.description="";
+			}	
 		},
 		cancelTask: function(e) {
 			e.preventDefault();
@@ -69,70 +75,3 @@ new Vue({
 
 
 })
-
-
-
-
-
-
-
-
-//new Vue({
-// 	el: "#app",
-// 	data: {
-// 		task: "",
-// 		description: "",
-// 		edit: false,
-// 		delete: false,
-// 		cancel: false,
-// 		id2: 0,
-// 		ind: 0,
-// 		tasks: []
-// 	},
-// 	methods: {
-// 		addTask: function(t,d,e) {
-// 			e.preventDefault();
-
-// 			this.tasks.push({
-// 				task: this.task,
-// 				description: this.description
-// 			});
-// 			localStorage.setItem("tasks", JSON.stringify(this.tasks));
-// 			this.task = "";
-// 			this.description = "";
-
-// 		},
-// 		editTask: function(t, i) {
-// 			this.edit = !this.edit;
-// 			this.task = t.task;
-// 			this.description = t.description;
-// 			this.ind = i;
-
-// 		},
-// 		updateTask: function(e) {
-// 			e.preventDefault();
-// 			this.edit = !this.edit;
-// 			 this.tasks[this.ind] = {task: this.task, description: this.description};
-// 			localStorage.setItem("tasks", JSON.stringify(this.tasks));
-// 			this.task = "";
-// 			this.description = "";
-			
-// 		},
-// 		cancelTask: function(e) {
-// 			e.preventDefault();
-// 			this.task = "";
-// 			this.description = "";
-// 			this.edit = !this.edit;
-// 		},
-// 		deleteTask: function(i) {
-// 			this.tasks.splice(i,1);
-// 			localStorage.setItem("tasks", JSON.stringify(this.tasks));
-// 		}
-// 	},
-// 	created: function() {
-// 		this.tasks = JSON.parse(localStorage.getItem("tasks"));
-
-// 	}
-
-
-// });
